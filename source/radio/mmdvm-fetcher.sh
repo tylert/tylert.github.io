e5c4c37b747dbfa679aefd9bbabb5dabe94b5b24
Tyler Tidman <tyler.tidman@draak.ca>
2021-05-30 20:53:43 -0400

    Stow the MMDVM cloner script

---
+++ source/radio/mmdvm-fetcher.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+# Non-git stuff...
+# wget -O stm32cube_fw_f1_v140.zip http://dvswitch.org/files/HAM/MMDVM/stm32cube_fw_f1_v140.zip
+
+# MMDVM/STM32 stuff...
+# https://sourceforge.net/projects/stm32flash/
+git clone https://git.code.sf.net/p/stm32flash/code stm32flash-code
+git clone https://github.com/g4klx/MMDVM.git
+git clone https://github.com/g4klx/MMDVMCal.git
+git clone https://github.com/g4klx/MMDVMHost.git
+git clone https://github.com/juribeparada/STM32F4XX_Lib.git
+git clone https://github.com/N4IRS/MMDVM-Install.git
+
+# DMR stuff...
+git clone https://github.com/g4klx/DMRGateway.git
+# git clone https://github.com/juribeparada/YSF2DMR.git
+# git clone https://github.com/juribeparada/NXDN2DMR.git
+
+# YSF stuff...
+# git clone https://github.com/g4klx/YSFClients.git
+# git clone https://github.com/juribeparada/DMR2YSF.git
+
+# DSTAR stuff...
+# git clone https://github.com/g4klx/DStarRepeater.git
+# git clone https://github.com/g4klx/ircDDBGateway.git
+
+# NXDN stuff...
+# git clone https://github.com/g4klx/NXDNClients.git
+# git clone https://github.com/juribeparada/DMR2NXDN.git
+# git clone https://github.com/juribeparada/YSF2NXDN.git
+
+# P25 stuff...
+# git clone https://github.com/g4klx/P25Clients.git
+# git clone https://github.com/juribeparada/YSF2P25.git
+
+# POCSAG stuff...
+# git clone https://github.com/g4klx/DAPNETGateway.git
+
+# PiStar stuff...
+# XXX XXX XXX git clone https://github.com/AndyTaylorTweet/Pi-Star_Binaries.git
+# XXX XXX XXX git clone https://github.com/AndyTaylorTweet/Pi-Star_v4_Binaries_Bin.git
+# git clone https://github.com/AndyTaylorTweet/Pi-Star_Binaries_sbin.git
+# git clone https://github.com/g4klx/DummyRepeater.git
+# XXX XXX XXX git clone https://github.com/marrold/AMBEServer.git
+
+# Other stuff...
+# git clone https://github.com/g4klx/AMBETools.git
+# git clone https://github.com/n8ohu/DMRRepeater
+
+# Maybe...
+# https://github.com/DVSwitch?tab=repositories
+# https://github.com/N4IRS?tab=repositories
