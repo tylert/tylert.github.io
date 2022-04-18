595912711f7ca99f1e12c51fe6223775794d0ded
Tyler Tidman <tyler.tidman@draak.ca>
2022-04-18 14:36:50 -0400

    Improve awesomeness for asset tag stickers

--- source/snippets/asset_tag.py
+++ source/snippets/asset_tag.py
@@ -1,27 +1,34 @@
 #!/usr/bin/env python
 
-import uuid
+from uuid import uuid4
 
-from shortuuid import ShortUUID
 import click
+from shortuuid import ShortUUID
 
 
 @click.command()
-@click.option('--alphabet', '-a',
-              default='123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz',
-              help='Alphabet to use with UUID shortener (default base58)')
-@click.option('--decode', '-d', default=None,
-              help='Decode this string back to a UUID')
+@click.option(
+    '--alphabet',
+    '-a',
+    default='123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz',
+    help='Alphabet to use with UUID shortener (default base58)',
+)
+@click.option(
+    '--decode',
+    '-d',
+    default=None,
+    help='Decode this string back to a UUID',
+)
 @click.help_option('--help', '-h')
 def main(alphabet, decode):
-    '''Main function'''
+    ''' '''
 
     shortening = ShortUUID(alphabet=alphabet)
 
     if decode:
         print(shortening.decode(decode))
     else:
-        print(shortening.encode(uuid.uuid4()))
+        print(shortening.encode(uuid4()))
 
 
 if __name__ == '__main__':
--- source/snippets/generate_labels.py
+++ source/snippets/generate_labels.py
@@ -1,69 +1,90 @@
 #!/usr/bin/env python
 
+
 from uuid import uuid4
+
 # import io
 
+import click
+from reportlab.graphics import renderPDF
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
 from shortuuid import ShortUUID
 import segno
 from svglib.svglib import svg2rlg
-from reportlab.lib.pagesizes import letter
-from reportlab.pdfgen import canvas
-from reportlab.graphics import renderPDF
-import click
 
 
 def get_short_uuid(alphabet):
-    ''''''
+    ''' '''
 
     return ShortUUID(alphabet=alphabet).encode(uuid4())
 
 
 def dump_qr_code(shortuuid, filename):
+    ''' '''
+
     segno.make(shortuuid).save(filename)
 
 
 @click.command()
-@click.option('--alphabet', '-a',
-              default='123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz',
-              help='Alphabet to use with UUID shortener (default base58)')
-@click.option('--thingy', '-t', default=None,
-              help='Filename to write PDF into')
-@click.option('--whatzit', '-w', default=None,
-              help='Filename to write SVG into')
+@click.option(
+    '--alphabet',
+    '-a',
+    default='123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz',
+    help='Alphabet to use with UUID shortener (default base58)',
+)
+@click.option(
+    '--banner',
+    '-b',
+    default='Evil Corp',
+    help='Banner to stamp on tags (default "Evil Corp")',
+)
+@click.option(
+    '--pdf',
+    '-p',
+    default='moo.pdf',
+    help='Filename of output PDF (default "moo.pdf")',
+)
+@click.option(
+    '--svg',
+    '-s',
+    default='moo.svg',
+    help='Filename of output SVG (default "moo.svg")',
+)
 @click.help_option('--help', '-h')
-def main(alphabet, thingy, whatzit):
-    '''Main function'''
+def main(alphabet, banner, pdf, svg):
+    ''' '''
 
     # buff = io.BytesIO()
     # svg = segno.make(a_short_uuid).save(buff, kind='svg', xmldecl=False,
     #                                     svgns=False)
 
-    canv = canvas.Canvas(thingy, pagesize=letter)
+    canv = canvas.Canvas(pdf, pagesize=letter)
 
     a_short_uuid = get_short_uuid(alphabet)
-    dump_qr_code(a_short_uuid, whatzit)
-    drawing = svg2rlg(whatzit)
+    dump_qr_code(a_short_uuid, svg)
+    drawing = svg2rlg(svg)
     renderPDF.draw(drawing, canv, 35, 723)
     canv.setFont('Courier', 12)
-    canv.drawString(70, 740, 'asset.ninja')
+    canv.drawString(70, 740, banner)
     canv.setFont('Courier', 8)
     canv.drawString(70, 730, a_short_uuid)
 
     a_short_uuid = get_short_uuid(alphabet)
-    dump_qr_code(a_short_uuid, whatzit)
-    drawing = svg2rlg(whatzit)
+    dump_qr_code(a_short_uuid, svg)
+    drawing = svg2rlg(svg)
     renderPDF.draw(drawing, canv, 35, 673)
     canv.setFont('Courier', 12)
-    canv.drawString(70, 690, 'asset.ninja')
+    canv.drawString(70, 690, banner)
     canv.setFont('Courier', 8)
     canv.drawString(70, 680, a_short_uuid)
 
     a_short_uuid = get_short_uuid(alphabet)
-    dump_qr_code(a_short_uuid, whatzit)
-    drawing = svg2rlg(whatzit)
+    dump_qr_code(a_short_uuid, svg)
+    drawing = svg2rlg(svg)
     renderPDF.draw(drawing, canv, 35, 623)
     canv.setFont('Courier', 12)
-    canv.drawString(70, 640, 'asset.ninja')
+    canv.drawString(70, 640, banner)
     canv.setFont('Courier', 8)
     canv.drawString(70, 630, a_short_uuid)
 
