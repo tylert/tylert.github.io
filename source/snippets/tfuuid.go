d8119315a106adb3c6bc8fde923d419662bb0194
Tyler Tidman <tyler.tidman@draak.ca>
2022-05-06 08:59:02 -0400

    Use a less crufty shortuuid library for Go

--- source/snippets/tfuuid.go
+++ source/snippets/tfuuid.go
@@ -2,18 +2,31 @@ package main
 
 import (
 	"fmt"
-	"github.com/anarcher/shortuuid"
+	"github.com/btcsuite/btcutil/base58"
+	"github.com/google/uuid"
+	"github.com/lithammer/shortuuid"
 )
 
-// https://github.com/anarcher/shortuuid
-// https://pkg.go.dev/github.com/anarcher/shortuuid
+// https://github.com/lithammer/shortuuid
+// https://pkg.go.dev/github.com/lithammer/shortuuid
 
 // https://github.com/skorokithakis/shortuuid
 // https://pypi.org/project/shortuuid/
 
+type base58Encoder struct{}
+
+func (enc base58Encoder) Encode(u uuid.UUID) string {
+	return base58.Encode(u[:])
+}
+
+func (enc base58Encoder) Decode(s string) (uuid.UUID, error) {
+	return uuid.FromBytes(base58.Decode(s))
+}
+
 func main() {
 	// default alphabet '23456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz' (base57)
 	// desired alphabet '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz' (base58)
-	id := shortuuid.NewWithAlphabet("123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz")
+	enc := base58Encoder{}
+	id := shortuuid.NewWithEncoder(enc)
 	fmt.Println(id)
 }
