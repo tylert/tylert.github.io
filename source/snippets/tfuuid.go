dd3f4413ed279b21b698a1a02e489a896be9618b
Tyler Tidman <tyler.tidman@draak.ca>
2022-06-02 16:42:37 -0400

    Touch up the tfuuid magic a bit more

--- source/snippets/tfuuid.go
+++ source/snippets/tfuuid.go
@@ -1,63 +1,80 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	// "os"
+	"os"
 
 	"github.com/btcsuite/btcutil/base58"
 	"github.com/google/uuid"
-	"github.com/lithammer/shortuuid"
+	// "github.com/nicksnyder/basen"
 )
 
 type base58Encoder struct{}
 
-func (enc base58Encoder) Encode(u uuid.UUID) string {
-	return base58.Encode(u[:])
+func (enc base58Encoder) Encode(uu uuid.UUID) string {
+	return base58.Encode(uu[:])
 }
 
-func (enc base58Encoder) Decode(s string) (uuid.UUID, error) {
-	return uuid.FromBytes(base58.Decode(s))
+func (enc base58Encoder) Decode(str string) (uuid.UUID, error) {
+	return uuid.FromBytes(base58.Decode(str))
+}
+
+func genv3(name string, space string) (uuid.UUID, error) {
+	switch space {
+	case "DNS":
+		return uuid.NewMD5(uuid.NameSpaceDNS, []byte(name)), nil
+	case "OID":
+		return uuid.NewMD5(uuid.NameSpaceOID, []byte(name)), nil
+	case "URL":
+		return uuid.NewMD5(uuid.NameSpaceURL, []byte(name)), nil
+	case "X500":
+		return uuid.NewMD5(uuid.NameSpaceX500, []byte(name)), nil
+	default:
+		return uuid.Nil, errors.New("Invalid namespace")
+	}
+}
+
+func genv4() (uuid.UUID, error) {
+	uu, err := uuid.NewRandom()
+	return uu, err
+}
+
+func genv5(name string, space string) (uuid.UUID, error) {
+	switch space {
+	case "DNS":
+		return uuid.NewSHA1(uuid.NameSpaceDNS, []byte(name)), nil
+	case "OID":
+		return uuid.NewSHA1(uuid.NameSpaceOID, []byte(name)), nil
+	case "URL":
+		return uuid.NewSHA1(uuid.NameSpaceURL, []byte(name)), nil
+	case "X500":
+		return uuid.NewSHA1(uuid.NameSpaceX500, []byte(name)), nil
+	default:
+		return uuid.Nil, errors.New("Invalid namespace")
+	}
 }
 
 func main() {
 	// default alphabet '23456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz' (base57)
 	// desired alphabet '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz' (base58)
 
-	// Generate a new short UUIDv4
 	enc := base58Encoder{}
-	id := shortuuid.NewWithEncoder(enc)
+	// uu, err := genv3("python.org", "DNS")
+	uu, err := genv4()
+	// uu, err := genv5("python.org", "DNS")
 
-	// Encode (shorten) an existing UUID
-	// uu, err := uuid.Parse("8966e5ee-445e-401b-a921-bf5020c516d2")
-	// if err != nil {
-	//   os.Exit(1)
-	// }}
-	// enc := base58Encoder{}
-	// id := enc.Encode(uu)
-	// Hy5v2PaRjQhVB172zb6fpD
+	// uu, err := uuid.Parse("cd5d0bff-2444-5d26-ab53-4f7db1cb733d")
+	// uu, err := enc.Decode("SMqCfPLDiH5aTTgLmGR4np")
 
-	// Decode (lengthen) an existing shortUUID
-	// enc := base58Encoder{}
-	// id, err := enc.Decode("Hy5v2PaRjQhVB172zb6fpD")
-	// if err != nil {
-	//   os.Exit(1)
-	// }}
-	// 8966e5ee-445e-401b-a921-bf5020c516d2
-
-	// Generate a UUIDv5
-	// name := []byte("python.org")
-	// uu := uuid.NewSHA1(uuid.NameSpaceDNS, []byte(name))
-	// uu := uuid.NewSHA1(uuid.NameSpaceOID, []byte(name))
-	// uu := uuid.NewSHA1(uuid.NameSpaceURL, []byte(name))
-	// uu := uuid.NewSHA1(uuid.NameSpaceX500, []byte(name))
-	// enc := base58Encoder{}
-	// id := enc.Encode(uu)
-
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	id := enc.Encode(uu)
 	fmt.Println(id)
 }
 
-// https://github.com/lithammer/shortuuid  go implementation
-// https://pkg.go.dev/github.com/lithammer/shortuuid  go implementation
 // https://github.com/skorokithakis/shortuuid  python implementation that is compatible
 // https://pypi.org/project/shortuuid/  python implementation that is compatible
 // https://github.com/yeqown/go-qrcode  generating a barcode bitmap
