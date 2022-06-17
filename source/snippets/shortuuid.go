4c783f559a467a0bcf1d60e5ef301a268a88948f
Tyler Tidman <tyler.tidman@draak.ca>
2022-06-17 13:03:39 -0400

    Almost got the base-n stuff working

--- source/snippets/shortuuid.go
+++ source/snippets/shortuuid.go
@@ -10,16 +10,6 @@ import (
 	// "github.com/nicksnyder/basen"
 )
 
-type base58Encoder struct{}
-
-func (enc base58Encoder) Encode(lluu uuid.UUID) string {
-	return base58.Encode(lluu[:])
-}
-
-func (enc base58Encoder) Decode(shuu string) (uuid.UUID, error) {
-	return uuid.FromBytes(base58.Decode(shuu))
-}
-
 func Genv3(name string, space string) (uuid.UUID, error) {
 	switch space {
 	case "DNS":
@@ -55,28 +45,42 @@ func Genv5(name string, space string) (uuid.UUID, error) {
 	}
 }
 
-// https://www.ietf.org/id/draft-peabody-dispatch-new-uuid-format-03.html
-// ^^ Genv6, Genv7, Genv8
+type base58Encoder struct{}
+
+func (enc base58Encoder) Encode(lluu uuid.UUID) string {
+	return base58.Encode(lluu[:])
+}
+
+func (enc base58Encoder) Decode(shuu string) (uuid.UUID, error) {
+	return uuid.FromBytes(base58.Decode(shuu))
+}
 
 func main() {
-	// default alphabet '23456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz' (base57)
-	// desired alphabet '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz' (base58)
+	// base57    '23456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+	// base58    '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+	// base62    '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+	// base64    '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/'
+	// base64url '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_'
 
 	// lluu, err := Genv3("python.org", "DNS")
 	// lluu, err := Genv4()
 	// lluu, err := Genv5("python.org", "DNS")
-	// lluu, err := uuid.Parse("cd5d0bff-2444-5d26-ab53-4f7db1cb733d")
+	lluu, err := uuid.Parse("cd5d0bff-2444-5d26-ab53-4f7db1cb733d")
+
 	enc := base58Encoder{}
-	lluu, err := enc.Decode("SMqCfPLDiH5aTTgLmGR4np")
+	// lluu, err := enc.Decode("SMqCfPLDiH5aTTgLmGR4np")
+	shuu := enc.Encode(lluu)
 
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	shuu := enc.Encode(lluu)
-	fmt.Println(shuu)
+
 	fmt.Println(lluu)
+	fmt.Println(shuu)
 }
 
 // https://github.com/yeqown/go-qrcode  generating a barcode bitmap
 // https://github.com/signintech/gopdf  generating a PDF
+// https://datatracker.ietf.org/doc/html/draft-peabody-dispatch-new-uuid-format-03
+// https://datatracker.ietf.org/doc/html/draft-msporny-base58-03
